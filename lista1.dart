8079314ec0d7ba01fdc9b0fefd4b5059c72102c1
Gabriel-Montes <a.22308051281276@cbtis128.edu.mx>
2025-02-11 17:00:47 +0000

    resultado

---
+++ lista1.dart
@@ -0,0 +1,15 @@
+void main() {  
+  print('Gabriel Alejandro Montes Hernandez Mat 22308051281276 gpo 6J');
+   
+  List<int> num = [10,20,30];  
+  print('Lista de numeros enteros');
+  print(num); 
+  print("Primer elemento: ${num[0]}");
+  print("Segundo elemento: ${num[1]}");
+  print("Tercer elemento: ${num[2]}");
+
+  print("Elementosde la lista usando ciclo for:");
+  for (int i = 0; i < num.length; i++) {  
+    print(num[i]);  
+  }
+}  
